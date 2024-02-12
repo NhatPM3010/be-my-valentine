02902cae31f30d9277c3493776a9a04f731bc618
NhatPM3010 <159754106+NhatPM3010@users.noreply.github.com>
2024-02-12 15:04:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function resizeYesButton() {
 
 function generateMessage(noCount) {
   const messages = [
-    "No",
+    "Thuiiii",
     "Are you sure?",
     "Pookie please",
     "Don't do this to me :(",
